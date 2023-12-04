8240b8caa67e0c8c88dabed1e5e69437445e5fef
NathanDeMaria <nathandmaria@gmail.com>
2023-12-04 03:42:30 +0000

    MC the extra credit

--- m_2023_12_03_oligopoly.py
+++ m_2023_12_03_oligopoly.py
@@ -3,25 +3,37 @@ import random
 from typing import Iterator
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from fire import Fire
 
 
 def _roll(n_dice: int) -> int:
     return sum(random.randint(1, 6) for _ in range(n_dice))
 
 
-def _simulate_board_run(board_length: int, n_dice: int = 2) -> Iterator[int]:
+def _simulate_board_run(board_length: int, n_dice: int) -> Iterator[int]:
     """Simulate a run around the board, returns all positions landed on."""
     current_location = 0
     while current_location < board_length:
-        current_location += _roll(n_dice)
         yield current_location
+        current_location += _roll(n_dice)
 
 
-def main(n_trials: int = 1_000_000, board_length: int = 40):
+def main(
+    n_trials: int = 1_000_000,
+    board_length: int = 40,
+    n_dice: int = 2,
+    min_board_square: int = 0,
+) -> None:
+    """
+    https://thefiddler.substack.com/p/can-you-race-around-the-monopoly
+
+    1 million takes ~30 seconds on my laptop
+    """
     counts: Counter[int] = Counter()
     for _ in tqdm(range(n_trials)):
-        for position in _simulate_board_run(board_length):
-            counts[position] += 1
+        for position in _simulate_board_run(board_length, n_dice=n_dice):
+            if position >= min_board_square:
+                counts[position] += 1
 
     positions = list(range(board_length))
     pcts = [counts[position] / n_trials for position in positions]
@@ -30,8 +42,10 @@ def main(n_trials: int = 1_000_000, board_length: int = 40):
     plt.ylabel("% of runs that hit this position")
     plt.ylim(0, 1)
     plt.savefig("oligopoly.png")
-    print(f"Max: {counts.most_common()[0]}")
+    frequencies = counts.most_common()
+    print(f"Max: {frequencies[0]}")
+    print(f"Min: {frequencies[-1]}")
 
 
 if __name__ == "__main__":
-    main()
+    Fire(main)
